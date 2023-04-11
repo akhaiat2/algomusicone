d0538a290eaa1eebfdbddaf36ce6e96bc37f52b3
Anthony Khaiat <92814726+akhaiat2@users.noreply.github.com>
2023-04-11 16:08:10 -0500

    Using Netnet code found here: https://netnet.studio/?layout=dock-left#code/eJytV2tv2zYU/Vz/ilsPqO3OktyiG9ogCZY1HVBgRQus25eiGCiJtplIpEZSUb0i/33nkrJs59GiWIHED/Hec89908cPk4Ter6WTJKwkv5ZUqdwKq6QjRYVpNvQjNcJ5WdLSmjqI/FqJf+U7A6XSFG0ttRdeGQ25laQkOR0dM+za+8YdZdlK+XWbp4WpMy+1M3ZZmS7zywuX1KaUlcu8lTKr2YjNGsAmpfSyYMjM2SLL2Rw//7uWpRKNanorrrCq8QSZk/HWWlHq9MIBVl3ZVEuf6abOfhkUA/749DiLut8EcpN9Yez3gspFcSl1mXQyX1X/G3Mb18NYHsCGBEmk/PWkJlU3xnqlVyG7742WgO8LYUPCUSerit9hC38TB9WqkXYQuTcbHlgXLu1LQJksb1VVZr2Jex3VOq2VvlvidESUPaZVZXJREbt4vvVwHrjPSWt6nEGskp6i+8bO6UqV0szJuAJHDJLBXeVo2eqgTYWVwqPuRRQlWUmuberWqliTQ5WK2pFpLeKRF6KmpURXCI3/skS7ePImRJD7IBro0FfuElJvG6nPXiNyBVLziQU7q7y8wWAaTXWhEzVZuRTMna1Qrvwsgm6D21TCL42tUwNwoUKHBfjkQlyJGK3EgV9uPkFTuI0udrbA+S92czqjzzglMNPO966fDJ2dxqi8irGYTsL5ZBY0wufUSX/mvVV56+V0IlpvQGwzmdPkC2I1XsuvyDCMU7pSWu5JDsRyU25S0cD38uUaVTUNGLM9X2LK4IzoBLKjxZVaCcQzDePgXF6pQqI4pf/TSfuGn00/B5Aj8raV1wfUbPE2v0ApAa7HpVBGhDT51uoohwfX95RXZQSqhOvj7DWFDr2dFASgbd7wGQ154Yxzp6IqGlVc9uXYWJl4KxCbMoLxeSd0KMLWyb0wxOOTw15J/2gb7npZBnMuHYb6TaOBEzlTSxoDcKlWrQ3zfsxHPDbcnrFtv70MkrAanUCUWu1VLY9oMgzjybw/c6ZqGfGd8GsIfMssnwSI6y1p8EUb5AhLiLTvDMcj1vA29mNwZIrjIe538B+q5jBqEem8F5qG0M5vOD3br4rt0UFh8Awprej2Ol9hUmhVi75UTDO7XR1BZb8u4O1SWbBGwoN3A3u4OaghAKiWUqF22fA4rEDKhUPp8DH0Ytcvrajl6MGDGAkWc0MYtsipdJ5phlJx+z2nljQNOmkl9cqv6ZQW4Mo8cdTHPsK4CJ6maV8A0ZeVFXkQfPKCppVc+r+VxjibhRn7dEFTq1br4eGl3DRGae8OMArBXST7LhlkeJZiYfHcje0jLKIRaTEXUjEQmC7uaAf4Pe4wPwz3l2SgkyBhZaJ0MhRzMkgF630OOAh957oPi4/pAPDhyYuPTDAIhICgEvRK2p57VA/xulv/6SLoR4k7APo3TirbSDd0ekILevSI+q/HJ/Ts+WI29PewQAzPGt7goulV57SYs/CckicLvP4063UeYBmnUMBAT69E1Upo4mt/eo0NjMzsDNwhzYBb8UPmkc3Syn92dIK36afA5+dnTIpfnsON0c4Cq7RSF5vBCD8Zje6gFEu747LREx/qLoR69HW+1/2QgCnnz7ad/xt34JT7fBYGxuieFXFzCvQDbor05HKFWubdiXTODm50b12hqkrEaRTocWhkFwV2p9NZ6s05N7oOrPDdeTTMlEndlTP2ipmAG65BuOXEc4Sl05Rv0PVL0VZ+NKQnLEfccHbUeTIdXLt2u3eYQbsryxdA9iYfs701z3cr9jZMZVb9Na4Wl9h8bf+bKG5QXuAYmbj2W0BWm2FrmEqmUJ1OBmOgU/IVaPtgNrrteHQJfABRIm24ysEkx5lv4uzK4UII+mD/HlvUtD5UyZwQeTThcNtQfH1d49fEw605DFn4g0sOfuGZFpbYobEVBW9KXSrGd2MoulbGmiPqMBBMlyLer66Qit8VxpqWdjrhEMCtEMPZaDRczv8DkCCxbA==

--- js/main.js
+++ js/main.js
@@ -1,30 +1,73 @@
-const ctx = new window.AudioContext()
-function play (e) {
-  const osc = ctx.createOscillator()
+import * as poseDetection from '@mediapipe/pose'
+import * as tf from '@tensorflow/tfjs-core'
+import '@tensorflow/tfjs-backend-webgl'
+import * as poseDetectionModel from '@tensorflow-models/pose-detection'
+import * as Tone from 'tone'
+import * as nn from './nn.min.js'
 
-  osc.type = 'square'
+let detector, video, osc
 
-  if (e.key === '1') {
-    osc.frequency.value = 261.63 // C
-  } else if (e.key === '2') {
-    osc.frequency.value = 293.66 // D
-  } else if (e.key === '3') {
-    osc.frequency.value = 329.63 // E
-  } else if (e.key === '4') {
-    osc.frequency.value = 349.23 // F
-  } else if (e.key === '5') {
-    osc.frequency.value = 392.00 // G
-  } else if (e.key === '6') {
-    osc.frequency.value = 440.00 // A
-  } else if (e.key === '7') {
-    osc.frequency.value = 493.88 // B
-  } else {
-    osc.frequency.value = Math.random() * 800
-  }
-
-  osc.connect(ctx.destination)
-  osc.start(ctx.currentTime)
-  osc.stop(ctx.currentTime + 0.25)
+async function addVideo () {
+  const video = document.createElement('video')
+  video.setAttribute('autoplay', '')
+  video.setAttribute('muted', '')
+  video.setAttribute('playsinline', '')
+  document.body.appendChild(video)
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true })
+  video.srcObject = stream
+  return video
 }
 
-window.addEventListener('keypress', play)
+async function setupModel () {
+  // here we pick which pre-trained model we want to use
+  const model = poseDetection.SupportedModels.BlazePose
+  // here we setup some "configuration" settings
+  const detectorConfig = {
+    runtime: 'mediapipe',
+    solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/pose'
+  }
+  // we combine the two to create the AI "detctor" function
+  const detector = await poseDetection.createDetector(model, detectorConfig)
+  return detector
+}
+
+async function setup () {
+  // we create (&& begin playing) the Tone.js Oscillator
+  osc = new Tone.Oscillator().toDestination().start()
+  osc.volume.value = -100 // set it's volume down by default
+  
+  // then we create our video element
+  video = await addVideo()
+  // then we create our AI function
+  detector = await setupModel()
+  // then we log it to make sure the model loaded correctly
+  console.log('detector ready', detector)
+  // then we wait a second before starting our animation loop
+  setTimeout(draw, 1000) // this is a hack!
+  // ...to get around the "race conditions" issue
+}
+async function draw () {
+  // we first use the detector AI function to predict our "pose" based on the video frame
+  const poses = await detector.estimatePoses(video)
+  if (poses.length > 0) { // if the AI detects poses...
+    // we grab the 19 (left_index) and 20 (right_index) keypoints
+    // we can see which keypoints refer to which part of the pose in the docs:
+    // https://github.com/tensorflow/tfjs-models/tree/master/pose-detection#blazepose-keypoints-used-in-mediapipe-blazepose
+    const left = poses[0].keypoints[19] // left index finger
+    const right = poses[0].keypoints[20] // right index finger
+    if (left.y >= 0 && left.y <= 480) {
+      const vol = nn.map(left.y, 0, 480, -100, 5)
+      osc.volume.value = vol
+    } else {
+      osc.volume.value = -100
+    }
+    const freq = nn.map(right.x, 0, 640, 440, 880)
+    osc.frequency.value = freq
+  } else {
+    // if we don't see poses
+    osc.volume.value = -100
+  }
+  window.requestAnimationFrame(draw)
+}
+
+window.addEventListener('load', setup)
